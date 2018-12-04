8cffce8b8f8c03dbe06e5d15def05d584af88dfa
Rick Lubbers <ik@ricklubbers.nl>
2018-12-04 16:19:58 +0100

    Remove useless logging

--- 1/a.js
+++ 1/a.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 
 console.log(fs.readFileSync('input.txt', { encoding: 'utf-8' })
 				.split('\n')
-				.map(str => { console.log(str); return str; })
 				.map(str => str.trim())
 				.map(str => parseInt(str, 10))
 				.filter(num => !isNaN(num) && num != null)
